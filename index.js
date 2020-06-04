c5f75a9846748d0069f6cf6f26246ab4d15498d8
adnanbarkati <66132376+adnanbarkati@users.noreply.github.com>
2020-06-04 21:33:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+alert("Asalamualekum") 
+alert("MADINA PAK is My World") //to view pop up on explorer
+alert("Welcome Muhammad Hannan Raza Madni")
+alert("To your Website")  
+console.log("Madina PAK my world") //to view result at nodejs by terminal
+document.write("Madina Pak is best") //only for view result on developer side
+
+var age = 45;  //assining variable
+var nationality = "Madni Raza";  //assining variable
+var iqamafree = true  //assining variable
+var a;
+a = 67; //to print variable 
+
+alert(nationality); 
+console.log(age);
+age = 67; //to print variable age with value 67
+console.log(age);
+console.log(iqamafree);
+
